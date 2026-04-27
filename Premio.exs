5f94fb24baef7401aae1ed10406bc5a0e92d3a45
JuanSZC <juans.zambranoc@uqvirtual.edu.co>
2026-04-27 09:50:49 -0500

    General Struct

--- Premio.exs
+++
@@ -1,9 +0,0 @@
-defmodule Premio do
-  @derive  {Jason.Encoder, only: [:id, :nombre, :valor, :sorteo_id]}
-
-  defstruct [:id, :nombre, :valor, :sorteo_id]
-
-  def crear(id, nombre, valor, sorteo_id) do
-    %Azar.Premio {id: id, nombre: nombre, valor: valor, sorteo_id: sorteo_id}
-  end
-end
